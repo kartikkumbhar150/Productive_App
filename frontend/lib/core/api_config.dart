3b606d790f34218c3fcaa6bb90d3b737a6fbefc5
Kartik Krishna Kumbhar <kumbharkartik150@gmail.com>
2026-04-01 03:00:12 +0530

    Fixed api

--- frontend/lib/core/api_config.dart
+++ frontend/lib/core/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
-  static const String baseUrl = 'http://192.168.1.103:5000/api'; 
+  static const String baseUrl = 'https://clario-track-your-time.vercel.app/api'; 
   // Note: if you use the Android Emulator, change this to 'http://10.0.2.2:5000/api'
 }
